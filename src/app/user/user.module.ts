9be4f1a5e58a687e94eedadd3c56ff58797159f7
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-11-03 17:10:57 +0700

    doing detail profile

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HomeComponent} from "./home/homepage/home.component";
 import {UserRoutingModule} from "./user-routing.module";
-import { ProfileComponent } from './home/profile/profile-detail/profile.component';
+import {ProfileDetailComponent} from './home/profile/profile-detail/profile-detail.component';
 
 @NgModule({
-  declarations: [HomeComponent, ProfileComponent],
+  declarations: [HomeComponent, ProfileDetailComponent],
   imports: [
     CommonModule,
     UserRoutingModule
   ]
 })
-export class UserModule { }
+export class UserModule {
+}
