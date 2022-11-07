9e538e89d41fd5084817c6abb90d9dd27ab161b5
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-11-07 16:10:14 +0700

    doing payment method

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {Category} from "../user/model/category";
+import {PaymentService} from "../user/service/paymentservice";
 
 @Component({
   selector: 'app-payment',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
+  paymentForm = new FormGroup({
+    time: new FormControl(),
+    totalSpent: new FormControl(),
+    note: new FormControl(),
+  })
 
-  constructor() { }
+  category: any;
+  color: string = '#E9E612';
+  nameCategory: string = 'Danh mục giao dich';
+  expenseCategories: Category[] = [];
+  incomeCategories: Category[] = [];
+  payment: any;
+  constructor(
+    private paymentService: PaymentService,
+    // private categoryService: CategoryService,
+    // private toast: NgToastService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  getCategory(id: number) {
+    this.categoryService.findById(id).subscribe(category => {
+      this.category = category;
+      this.nameCategory = category.name;
+      this.color = category.color;
+    })
   }
 
 }
