408b7c5fcbcea70df01f2b46b7c06369926d57e3
Hiep Nguyen <gagga5376@gmail.com>
2022-11-07 10:08:16 +0700

    show category lan 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
+import { ShowCategoryComponent } from './category/show-category/show-category.component';
+import { AddCategoryComponent } from './category/add-category/add-category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ShowCategoryComponent,
+    AddCategoryComponent,
 
   ],
   imports: [
