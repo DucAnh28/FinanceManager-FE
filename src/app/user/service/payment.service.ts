ca932a64f356af7663471a975e7d57fab967dbff
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-11-15 21:52:13 +0700

    share wallet service

--- src/app/user/service/payment.service.ts
+++ src/app/user/service/payment.service.ts
@@ -40,28 +40,7 @@ export class PaymentService {
     return this.httpClient.delete(API + id);
   }
 
-  // findAllByMonth(status: any): Observable<any> {
-  //   const id = localStorage.getItem("ID_WALLET");
-  //   return this.httpClient.get(API + `find-all-by-time?status=${status}&id=${id}`);
-  // }
 
-  // findAllTransactionsIncomeFor6Months(): Observable<any> {
-  //   const id = localStorage.getItem("ID_WALLET");
-  //   return this.httpClient.get(API + `find-all-income-6month/${id}`);
-  // }
-  //
-  // findAllTransactionsExpenseFor6Months(): Observable<any> {
-  //   const id = localStorage.getItem("ID_WALLET");
-  //   return this.httpClient.get(API + `find-all-expense-6month/${id}`);
-  // }
-  //
-  // findAllTransactions(startTime: any, endTime: any, status: any, from: any, to: any): Observable<any> {
-  //   const id = localStorage.getItem("ID_WALLET");
-  //   return this.httpClient.get(API + `find-all-transaction?startTime=${startTime}&endTime=${endTime}&status=${status}&from=${from}&to=${to}&id=${id}`);
-  // }
-  // findAllTransactionsByCategoryID(id: any): Observable<any> {
-  //   return this.httpClient.get(API + 'find-by-category/'+ id);
-  // }
   findAllPaymentByCategoryID(id: any): Observable<any> {
     return this.httpClient.get(API + 'find-by-category/'+ id);
   }
@@ -69,7 +48,5 @@ showpaymentintime(startDate:Date,endDate:Date):Observable<Payment[]>{
     return this.httpClient.get<Payment[]>(API+'find-All-Transactions-during-time?startDate='+startDate+'&endDate='+endDate)
 }
 
-// showPaymentInTimeByWallet(startDate:Date, endDate:Date):Observable<Payment[]>{
-//     return this.httpClient.get<Payment[]>(API + 'find-All-Transactions-during-time-by-wallet?startDate=' + startDate + '&endDate=' + endDate + '$walletid=' + en)
-// }
+
 }
