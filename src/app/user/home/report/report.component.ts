5c67655eb7b8e89056508d2edf6a4dc87c0acee3
Hiep Nguyen <gagga5376@gmail.com>
2022-11-16 14:51:18 +0700

    xong front end report

--- src/app/user/home/report/report.component.ts
+++ src/app/user/home/report/report.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {PaymentService} from "../../service/payment.service";
+import {Payment} from "../../model/payment";
+import {AccountService} from "../../../account/service/account.service";
+import {PaymentInDay} from "../../model/payment-in-day";
 
 @Component({
   selector: 'app-report',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent implements OnInit {
-
-  constructor() { }
+payment:PaymentInDay[]=[];
+userID : number =0;
+  constructor(
+    private paymentService: PaymentService,
+    private accountService: AccountService
+  ) {
+    accountService.getUserById().subscribe(data=>{
+      this.userID = data.id;
+      this.showPaymentToday();
+    })
+  }
 
   ngOnInit(): void {
+    // this.showPaymentToday();
+  }
+
+  showPaymentToday() {
+    this.paymentService.showpaymentToday(this.userID).subscribe(data=>{
+      this.payment = data
+    });
   }
 
 }
